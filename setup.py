6c730a29182b6c3290564dd3217bf70c6374dfbc
Tony Spataro <github.com@tracker.xeger.net>
2024-04-12 17:39:14 -0700

    v1.2.3

--- setup.py
+++ setup.py
@@ -2,13 +2,9 @@ from setuptools import setup
 
 setup(
     name='awsume-1password-plugin',
-    version='1.2.2',
+    version='1.2.3',
     description='Automates awsume MFA entry via 1Password CLI.',
-    entry_points={
-        'awsume': [
-            '1password = 1password'
-        ]
-    },
+    entry_points={'awsume': ['1password = 1password']},
     author='Tony Spataro',
     author_email='pypi@tracker.xeger.net',
     url='https://github.com/xeger/awsume-1password-plugin',
